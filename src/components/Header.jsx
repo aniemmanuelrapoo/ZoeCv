07ef2cbda773559c0e309046877b1156d31428fc
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-10-16 19:58:45 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,9 @@
-
-
-const Header = ({title, image, children}) => {
+const Header = ({title, image1, children}) => {
   return (
     <header className="header">
       <div className="header__container">
         <div className="header__container-bg">
-          <img src={image} alt="Header Background image" />
+          <img src={image1} alt="Header Background image1" />
         </div>
         <div className="header__content">
           <h2>{title}</h2>
